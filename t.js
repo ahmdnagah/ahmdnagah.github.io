d3ccaf8121c2e8b44650fab79eccefc0d12efec8
ahmdnagah <58993471+ahmdnagah@users.noreply.github.com>
2020-01-02 20:43:01 +0200

    creat main site structure

---
+++ t.js
@@ -0,0 +1,50 @@
+
+
+
+var m = document.getElementById("monster");
+var m2 = document.getElementById("monster2");
+var m3 = document.getElementById("monster3");
+var m4 = document.getElementById("monster4");
+var m5 = document.getElementById("monster5");
+var m6 = document.getElementById("monster6");
+var m7 = document.getElementById("monster7");
+var m8 = document.getElementById("monster8");
+var m9 = document.getElementById("monster9");
+var m10 = document.getElementById("monster10");
+var m11 = document.getElementById("monster11");
+var m12 = document.getElementById("monster12");
+var m13 = document.getElementById("monster13");
+var m14 = document.getElementById("monster14");
+var m15 = document.getElementById("monster15");
+var m16 = document.getElementById("monster16");
+var m17 = document.getElementById("monster17");
+var m18 = document.getElementById("monster18");
+
+
+
+
+
+var dd=document.getElementById("d");
+dd.onkeyup = function(){
+    m.textContent=this.value;
+   m2.textContent=this.value;
+     m3.textContent=this.value ;
+     m4.textContent=this.value;
+     m5.textContent=this.value;
+   m6.textContent=this.value;
+     m7.textContent=this.value ;
+     m8.textContent=this.value;
+     m9.textContent=this.value;
+   m10.textContent=this.value;
+     m11.innerHTML=this.value ;
+     m12.textContent=this.value;
+     m13.textContent=this.value;
+   m14.textContent=this.value;
+     m15.textContent=this.value ;
+     m16.textContent=this.value;
+     m17.textContent=this.value ;
+     m18.textContent=this.value;
+    
+    
+    
+}
